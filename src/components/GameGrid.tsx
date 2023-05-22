3277040f439f7dfac01e9cbeede05b75813766cf
Abdul Shakur Abbas Sakhr <76248149+Algo-master@users.noreply.github.com>
2023-05-22 16:49:25 +0000

    Create customer hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,29 +1,8 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface GamesData {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<GamesData>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-
-    return () => console.log("unmounted");
-  }, []);
+	const { games, error } = useGames();
 
   return (
     <>
