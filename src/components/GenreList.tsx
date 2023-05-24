0bd4e3561805f86964dd6bbc95bf66271dbd9087
Abdul Shakur Abbas Sakhr <76248149+Algo-master@users.noreply.github.com>
2023-05-24 16:49:57 +0000

    cleanup the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import useGenres, { Genre } from "../hooks/useGenres";
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
-interface Props{
+interface Props {
   onSelectGenre: (genre: Genre) => void;
   selectedGenre: Genre | null;
 }
@@ -11,25 +20,45 @@ interface Props{
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
-	if(isLoading) return <Spinner />
+  if (isLoading) return <Spinner />;
 
-	if(error) return null;
+  if (error) return null;
 
-  return ( 
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px" bgColor={ genre.id === selectedGenre?.id ? 'whiteAlpha.100' : 'none'} borderRadius={10}>
-          <HStack>
-            <Image
-              src={getCroppedImageUrl(genre.image_background)}
-              borderRadius={8}
-              boxSize="32px"
-            />
-            <Button fontWeight={ genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize='large' variant='link'>{genre.name}</Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+  return (
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {data.map((genre) => (
+          <ListItem
+            key={genre.id}
+            paddingY="5px"
+            bgColor={genre.id === selectedGenre?.id ? "whiteAlpha.100" : "none"}
+            borderRadius={10}
+          >
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(genre.image_background)}
+                borderRadius={8}
+                boxSize="32px"
+                objectFit="cover"
+              />
+              <Button
+                textAlign="left"
+                whiteSpace="normal"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                fontSize="large"
+                variant="link"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
